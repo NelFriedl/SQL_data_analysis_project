5ffe7cc0c42c8786bc888defbeb56f771c048227
NelFriedl <nela.friedl@gmail.com>
2025-09-02 23:43:36 +0200

    Update SQL_project_task_2.sql

--- SQL_project_task_2.sql
+++ SQL_project_task_2.sql
@@ -4,13 +4,8 @@
 WITH purchasing_power AS (
     SELECT
         t1.year,
-        t1.industry_name,
-        t1.industry_branch_code,
-        t1.average_payroll,
-        t1.average_price_value,
         t1.product_name,
-        t1.price_unit,
-        (average_payroll / average_price_value)::NUMERIC AS affordable_amount
+        AVG(average_payroll / average_price_value)::NUMERIC AS affordable_amount
     FROM
     	t_nela_friedlova_project_sql_primary_final AS t1
     WHERE
@@ -18,23 +13,14 @@ WITH purchasing_power AS (
         AND t1.category_code IN (111301, 114201)
     GROUP BY
         t1.year,
-        t1.average_payroll,
-        t1.industry_branch_code,
-        t1.industry_name,
-        t1.product_name,
-        t1.average_price_value,
-        t1.price_unit
+        t1.product_name
 )
 SELECT
     year,
-    industry_name,
-    average_payroll,
-    average_price_value,
-    product_name,
-    price_unit,    
+    product_name,  
     ROUND(affordable_amount) AS affordable_amount
 FROM
     purchasing_power
 ORDER BY
     year,
-    industry_name;
+    product_name;
